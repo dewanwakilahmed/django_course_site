83ec788a1b41dd22127f3077c2cfeee3dd186e9d
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2021-12-24 01:24:55 +0600

    Dynamic Paths
    - Added 'meetup_slug' parameter to 'meetup_detail' function in 'meetups' App
    - Added path for 'meetups_details' view and added 'meetup_slug' for dynamic url

--- meetups/views.py
+++ meetups/views.py
@@ -22,7 +22,7 @@ def index(request):
     })
 
 
-def meetup_details(request):
+def meetup_details(request, meetup_slug):
     selected_meetup = {
         'title': 'A First Meetup',
         'description': 'This is the first meetup!'
